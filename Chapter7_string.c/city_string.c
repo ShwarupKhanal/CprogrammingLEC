dcf9e1ed5c290664cd6e4cb9a2823748d3047a6e
ShwarupKhanal <purushottamkhanal201@gmail.com>
2022-03-28 09:57:08 +0545

    character_string

---
+++ Chapter7_string.c/city_string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i;
+    char name1[]="Pokhara city";
+    char name2[]={'k','a','t','h','m','a','n','d','u','c','i','t','y'};
+    for(i<0;i<strlen(name1);i++)
+    printf("%c \n",name1[i]);
+    for(i<0;i<strlen(name2);i++)
+    printf("%c \n",name2[i]);
+
+}
